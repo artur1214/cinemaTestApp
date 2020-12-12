3795fed0b2df90d8c18e94958850d1782fb218de
artur1214 <41078064+artur1214@users.noreply.github.com>
2020-12-12 14:31:22 +0600

    Update jinja_env.py

--- djangoProject/jinja_env.py
+++ djangoProject/jinja_env.py
@@ -6,7 +6,7 @@ from jinja2 import Environment
 def environment(**options):
     #print(vars(staticfiles_storage.__dict__['_wrapped']))
     env = Environment(**options)
-    print(staticfiles_storage.url('main/js/formCheck.js'))
+    #print(staticfiles_storage.url('main/js/formCheck.js'))
     env.globals.update(
         {'static': staticfiles_storage.url, 'url': reverse}
     )
